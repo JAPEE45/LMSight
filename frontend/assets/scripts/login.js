7ef3c3c49f507c29a7c5f0dd3f5f254c9d2a4093
chris <chrisbriollovehannah@gmail.com>
2025-08-13 20:11:44 +0800

    finished?

--- frontend/assets/scripts/login.js
+++ frontend/assets/scripts/login.js
@@ -8,7 +8,7 @@ loginBtn.addEventListener("click", (event) => {
     if(username.value === "admin" && password.value === "admin") {
         window.location.href = "admin_interface.html";
     } else if(username.value === "hr" && password.value === "hr") {
-        window.location.href = "hr_interface.html";
+        window.location.href = "hr_interface_dashboard.html";
     } else {
         window.location.href = "user_interface.html"
     }
