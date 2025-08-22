2831479a950030cce0b49078b98d80d90bfb8b54
chris <chrisbriollovehannah@gmail.com>
2025-08-22 13:06:25 +0800

    UI finished

--- assets/scripts/popups/sidebar.js
+++ assets/scripts/popups/sidebar.js
@@ -1,10 +1,9 @@
-// Mobile Sidebar Toggle
+
 function toggleSidebar() {
     const sidebar = document.getElementById('sidebar');
     sidebar.classList.toggle('show');
 }
 
-// Close sidebar when clicking outside on mobile
 document.addEventListener('click', function(event) {
     const sidebar = document.getElementById('sidebar');
     const toggleBtn = document.querySelector('.mobile-toggle');
@@ -15,3 +14,19 @@ document.addEventListener('click', function(event) {
         }
     }
 });
+
+const imageInput = document.getElementById("image_file");
+  const previewImg = document.getElementById("profilePreview");
+
+  imageInput.addEventListener("change", function () {
+    const file = this.files[0];
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = function (e) {
+        previewImg.src = e.target.result;
+      };
+
+      reader.readAsDataURL(file);
+    }
+  });
